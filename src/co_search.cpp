1a99ffa1c7e24b50827d6eaa32a24dcc20b35ab8
Maxim Sharipov <msharipovr@gmail.com>
2024-04-14 11:20:09 -0400

    feat(circles): add intersection finding

--- src/co_search.cpp
+++ src/co_search.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <wykobi/wykobi.hpp>
+#include <wykobi/wykobi_utilities.hpp>
+#include <wykobi/wykobi_algorithm.hpp>
+
+namespace wy = wykobi;
+namespace alg = wykobi::algorithm;
 
 int main(void) {
-  
-  std::cout << "Hello World\n";
+
+  typedef long double T;
+
+  wy::point2d<T> point1 = wy::make_point(0.0L, 0.0L);
+  wy::point2d<T> point2 = wy::make_point(1.0L, 0.0L);
+
+  wy::circle<T> c1 = wy::make_circle(0.0L, 0.0L, 1.0L);
+  wy::circle<T> c2 = wy::make_circle(1.0L, 0.0L, 1.0L);
+  std::vector<wy::circle<T>> circles = {c1, c2};
+  std::vector<wy::point2d<T>> inter;
+  alg::naive_group_intersections<wy::circle<T>>(circles.begin(), circles.end(),
+                                 std::back_inserter(inter));
+  for (auto p : inter) {
+    std::cout << p << " ";
+  }
+
+  std::cout << "\n";
   return 0;
 }
